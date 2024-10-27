014cd0d2eaedfaca93c890aa75d9e5cb51643ec5
TonFak <118545096+TonFak@users.noreply.github.com>
2024-10-28 00:35:22 +0200

    Update task3.py

--- lab_4/task3.py
+++ lab_4/task3.py
@@ -6,10 +6,10 @@ cap = cv2.VideoCapture(parser.parse_args().movie)
 while (True):
     ret, frame = cap.read()
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame,str(cap.get(cv2.CAP_PROP_FPS)),(50, 50),font, 1,(0, 255, 255),2, cv2.LINE_4)
-    cv2.putText(frame, parser.parse_args().movie, (150, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+    cv2.putText(frame,str(cap.get(cv2.CAP_PROP_FPS)),(50, 50),font, 1,(245, 122, 148),2, cv2.LINE_4)
+    cv2.putText(frame, parser.parse_args().movie, (150, 50), font, 1, (100, 255, 255), 2, cv2.LINE_4)
     cv2.imshow('video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(15) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
